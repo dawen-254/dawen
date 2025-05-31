03d3a12089da8b118d76ecb2c2cc310396274ad1
Dawen <obiladawen@gmail.com>
2025-05-31 12:40:07 +0300

    Fix

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -23,10 +23,10 @@ const ProductsPage = () => {
     exterior: [
       { id: 1, src: "/HOUSE1.jpg", alt: "Exterior 1" },
       { id: 2, src: "/HOUSE2.jpg", alt: "Exterior 2" },
-      { id: 3, src: "/HOUSE3.jpg", alt: "Exterior 3" },
+      { id: 3, src: "/HOUSE8.jpg", alt: "Exterior 3" },
       { id: 4, src: "/HOUSE4.jpg", alt: "Exterior 4" },
       { id: 5, src: "/HOUSE5.jpg", alt: "Exterior 5" },
-      { id: 6, src: "/HOUSE8.jpg", alt: "Exterior 6" }
+      { id: 6, src: "/HOUSE6.jpg", alt: "Exterior 6" }
     ]
   };
 
